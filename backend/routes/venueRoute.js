8ee23a2c6cbe32b11b835e6f53376d78d8027634
Aniket Arjun Khillare <95135993+aniket-017@users.noreply.github.com>
2023-07-21 13:53:58 +0530

    Delete backend/routes directory

--- backend/routes/venueRoute.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { getAllVenues, createVenue } = require('../controllers/venueController');
-
-const router = express.Router();
-
-router.route('/venues').get(getAllVenues);
-router.route('/venue/new').post(createVenue);
-
-module.exports = router;
